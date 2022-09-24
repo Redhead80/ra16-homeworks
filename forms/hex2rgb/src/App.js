f4e177d74f6f8d3dbe5efd38c81ba15132e9fe1c
Redhead80 <g.utkin80@yandex.ru>
2022-09-24 18:15:22 +0300

    hex2rgb

---
+++ forms/hex2rgb/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ConverterForm from './components/ConverterForm';
+
+function App() {
+  return (
+    <ConverterForm></ConverterForm>
+  );
+}
+
+export default App;
---
+++ forms/hex2rgb/src/components/ConverterForm.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function ConverterForm(props) {
+  const [form, setForm] = useState({
+    hexColor: '',
+  });
+  const [rgbColor, setRgbColor] = useState({
+    value: ''
+  })
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(form.hexColor);
+
+    setRgbColor(rgbColor => ({...rgbColor,
+      value: result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+                    : "Ошибка!"
+    }));
+  };
+
+  const handleColorChanged = (event) => {
+    setForm(prevForm => ({...prevForm, hexColor: event.target.value}));
+  };
+  
+  return (
+    <div className="converter" style={{backgroundColor: ((rgbColor.value === 'Ошибка!') ? 'crimson' : rgbColor.value)}}>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleColorChanged}></input>
+        <div className="rgb">{rgbColor.value}</div>
+      </form>
+    </div>
+  )
+}
+
+export default ConverterForm;
