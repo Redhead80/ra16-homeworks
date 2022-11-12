6f11c0daf161671b88e93f6a0c909b7dd400c083
Redhead80 <g.utkin80@yandex.ru>
2022-11-13 02:34:34 +0300

    fixed

--- redux/editing/src/store/index.js
+++ redux/editing/src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers } from "redux";
 import serviceListReducer from "../reducers/ServiceList";
 import serviceAddReducer from "../reducers/ServiceAdd";
 
